33c57d78159dd5d127b9107129fbe54a816f1eb3
Mauro <maurosacramento35@gmail.com>
2024-11-21 10:34:08 +0000

    Buscar estados e cidades API

---
+++ create-point.js
@@ -0,0 +1,20 @@
+
+//fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+//.then()
+
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]");
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json() )
+    .then( states => {
+        
+        for (const state of states){
+             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+       
+    })
+}
+
+populateUFs();
+
