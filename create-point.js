3e2e7a6eb699546cc37e4ceedbb1c4445f4f41ea
Mauro <maurosacramento35@gmail.com>
2024-11-21 13:45:11 +0000

    Complete populate city

--- create-point.js
+++ create-point.js
@@ -18,3 +18,26 @@ function populateUFs(){
 
 populateUFs();
 
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const value = event.target.value;
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${value}/municipios`
+
+    fetch(url)
+    .then(res => res.json() )
+    .then( cities => {
+        
+        for (const city of cities){
+             citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+       citySelect.disabled = false;
+    })
+}
+
+document.querySelector("select[name=uf]").addEventListener("change", getCities)
